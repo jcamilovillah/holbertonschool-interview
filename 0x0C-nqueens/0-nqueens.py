5b9c4290035707c56e5074b482a0249189d4fc0e
Juan Camilo Villa <villahjuancamilo@gmail.com>
2021-10-20 16:29:29 -0500

    change implementation

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,37 +1,59 @@
 #!/usr/bin/python3
-""" Module that solves the N queens problem """
-
-
+"""
+    The N queens puzzle is the challenge of placing N non-attacking
+    queens on an N×N chessboard. Write a program
+    that solves the N queens problem.
+"""
 import sys
 
+
+def n_q(t_arr, arr, col, i, n):
+    """
+    If the user called the program with the wrong number of arguments,
+    print Usage: nqueens N, followed by a new line, and exit with the status 1
+    """
+    if (i > n):
+        arr.append(t_arr[:])
+        return arr
+
+    for j in range(n + 1):
+        if i == 0 or ([i - 1, j - 1] not in t_arr and [i - 1, j + 1]
+                      not in t_arr and j not in col):
+            if i > 1:
+                dia = 0
+                for k in range(2, i + 1):
+                    if ([i - k, j - k] in t_arr) or ([i - k, j + k] in t_arr):
+                        dia = 1
+                        break
+                if dia:
+                    continue
+            t_arr.append([i, j])
+            col.append(j)
+            n_q(t_arr, arr, col, i + 1, n)
+            col.pop()
+            t_arr.pop()
+
+    return arr
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        sys.exit(1)
+        exit(1)
 
     try:
-        N = int(sys.argv[1])
-    except ValueError:
+        n = int(sys.argv[1])
+    except:
         print("N must be a number")
-        sys.exit(1)
+        exit(1)
 
-    if N < 4:
+    if not isinstance(n, int):
+        print("N must be a number")
+        exit(1)
+
+    elif n < 4:
         print("N must be at least 4")
-        sys.exit(1)
+        exit(1)
 
-
-def bTracking(N, i=0, a=[], b=[], c=[]):
-    """Generates backtracking of the answers of the puzzle """
-    if i < N:
-        for j in range(N):
-            if j not in a and i+j not in b and i-j not in c:
-                for ans in bTracking(N, i+1, a+[j], b+[i+j], c+[i-j]):
-                    yield ans  # iterator of the solutions
-    else:
-        yield a  # iterator of the first list with no answer
-
-
-for ans in bTracking(N):
-    resp = [[col, row] for col, row in enumerate(ans)]
-    print(resp)
+    n_q_arr = n_q([], [], [], 0, n - 1)
+    for i in n_q_arr:
+        print(i)
